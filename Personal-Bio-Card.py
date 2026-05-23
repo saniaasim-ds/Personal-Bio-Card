e11cc565d4a0df5ff0b4046167c03b5c065e2995
Sania Asim <saniaasim.ds@gmail.com>
2026-05-23 21:45:22 +0500

    Formatted Bio Card

---
+++ Personal-Bio-Card.py
@@ -0,0 +1,11 @@
+#Project-2
+Name = "Sania"
+Age = 20
+City = "Gujranwala"
+Hobby = "My hobby is doing editing , listening to music and videography"
+print("\n")
+print("\t\t\tPersonal Bio Card \n" )
+print(f"Name: {Name}")
+print(f"Age: {Age}")
+print(f"City: {City}")
+print(f"Hobby: {Hobby}")
